746c42ed6e770372cd7761b382f579ba0d67a5a6
rehohoho <ernie.birdy.ho@gmail.com>
2021-10-22 16:52:57 +0000

    Add tpn sthv2 config, fix tpn diving head config.

--- configs/recognition/tpn/tpn_slowonly_r50_8x8x1_150e_sthv2_rgb.py
+++ configs/recognition/tpn/tpn_slowonly_r50_8x8x1_150e_sthv2_rgb.py
@@ -18,10 +18,10 @@ model = dict(
           mid_channels=(1024, 1024),
           out_channels=2048,
           downsample_scales=((1, 1, 1), (1, 1, 1))),
-      aux_head_cfg=dict(out_channels=48, loss_weight=0.5)),
+      aux_head_cfg=dict(out_channels=174, loss_weight=0.5)),
   cls_head=dict(
       type='TPNHead',
-      num_classes=48,
+      num_classes=174,
       in_channels=2048,
       spatial_type='avg',
       consensus=dict(type='AvgConsensus', dim=1),
@@ -29,23 +29,25 @@ model = dict(
       init_std=0.01)
 )
 
-dataset_type = 'RawframeDataset'
-data_root = 'data/diving48/rawframes'
-data_root_val = 'data/diving48/rawframes'
-ann_file_train = 'data/diving48/diving48_train_list_rawframes.txt'
-ann_file_val = 'data/diving48/diving48_val_list_rawframes.txt'
-ann_file_test = 'data/diving48/diving48_val_list_rawframes.txt'
+dataset_type = 'Rawhdf5Dataset'
+data_root = 'data/ssv2'
+data_root_val = 'data/ssv2'
+ann_file_train = 'data/ssv2/ssv2_train_annotations.txt'
+ann_file_val = 'data/ssv2/ssv2_val_annotations.txt'
+ann_file_test = 'data/ssv2/ssv2_val_annotations.txt'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_bgr=False)
 train_pipeline = [
-    dict(type='SampleFrames', clip_len=8, frame_interval=8, num_clips=1), # sth1: 1, 1, 8
-    dict(type='RawFrameDecode'),
+    dict(type='SampleFrames', clip_len=8, frame_interval=8, num_clips=1),
+    # dict(type='RawFrameDecode'),
+    dict(type='RawHDF5Decode'),
+    dict(type='Resize', scale=(-1, 256)),
     dict(type='RandomResizedCrop'),
     dict(type='Resize', scale=(224, 224), keep_ratio=False),
     dict(type='Flip', flip_ratio=0.5),
     dict(type='ColorJitter'),
     dict(type='Normalize', **img_norm_cfg),
-    dict(type='FormatShape', input_format='NCTHW'), # sth1: nchw
+    dict(type='FormatShape', input_format='NCTHW'),
     dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
     dict(type='ToTensor', keys=['imgs', 'label'])
 ]
@@ -55,13 +57,13 @@ val_pipeline = [
         clip_len=8,
         frame_interval=8,
         num_clips=1,
-        test_mode=True), # sth1: 1, 1, 8
-    dict(type='RawFrameDecode'),
+        test_mode=True),
+    dict(type='RawHDF5Decode'),
     dict(type='Resize', scale=(-1, 256)),
     dict(type='CenterCrop', crop_size=224),
     dict(type='ColorJitter'),
     dict(type='Normalize', **img_norm_cfg),
-    dict(type='FormatShape', input_format='NCTHW'), # sth1: nchw
+    dict(type='FormatShape', input_format='NCTHW'),
     dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
     dict(type='ToTensor', keys=['imgs'])
 ]
@@ -72,7 +74,7 @@ test_pipeline = [
         frame_interval=8,
         num_clips=10,
         test_mode=True),
-    dict(type='RawFrameDecode'),
+    dict(type='RawHDF5Decode'),
     dict(type='Resize', scale=(-1, 256)),
     dict(type='ThreeCrop', crop_size=256),
     dict(type='Normalize', **img_norm_cfg),
@@ -81,9 +83,8 @@ test_pipeline = [
     dict(type='ToTensor', keys=['imgs'])
 ]
 data = dict(
-    videos_per_gpu=8,
-    workers_per_gpu=8,
-    test_dataloader=dict(videos_per_gpu=1),
+    videos_per_gpu=8, #8
+    workers_per_gpu=8, #8
     train=dict(
         type=dataset_type,
         ann_file=ann_file_train,
@@ -104,12 +105,12 @@ evaluation = dict(
 
 # optimizer
 optimizer = dict(
-    type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001,
+    type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001, # lr 0.01
     nesterov=True)  # this lr is used for 8 gpus
 optimizer_config = dict(grad_clip=dict(max_norm=40, norm_type=2))
 # learning policy
 lr_config = dict(policy='step', step=[75, 125])
-total_epochs = 150
+total_epochs = 150 # 150
 
 # runtime settings
-work_dir = './work_dirs/tpn_slowonly_r50_8x8x1_150e_diving48_rgb'  # noqa: E501
+work_dir = './work_dirs/tpn_slowonly_r50_8x8x1_150e_sthv2_rgb'  # noqa: E501
